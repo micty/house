b1e4bb68be2443b3f24a82e9536f13e333a79205
micty <79230533@qq.com>
2015-12-21 11:15:34 +0800

    CR+LF

--- admin/bin/tasks/watch-js.js
+++ admin/bin/tasks/watch-js.js
@@ -24,18 +24,18 @@ module.exports = function (pages) {
 
 
 
-    Tasks.add('watch', 'card-detail-js', {
-        files: [
-            '<%=dir.htdocs%>html/card-detail/**/*.js',
-        ],
-        tasks: [
-            'copy:card-detail-html', //这里用到 watch-html.js 中的任务
-        ],
-        options: {
-            spawn: false,
-            event: ['changed', 'added', 'deleted'], //监听事件
-        }
-    });
+    //Tasks.add('watch', 'card-detail-js', {
+    //    files: [
+    //        '<%=dir.htdocs%>html/card-detail/**/*.js',
+    //    ],
+    //    tasks: [
+    //        'copy:card-detail-html', //这里用到 watch-html.js 中的任务
+    //    ],
+    //    options: {
+    //        spawn: false,
+    //        event: ['changed', 'added', 'deleted'], //监听事件
+    //    }
+    //});
 
     pages.forEach(function (item, index) {
 
